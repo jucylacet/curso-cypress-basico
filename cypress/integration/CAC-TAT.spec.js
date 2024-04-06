1c2a0ad1c65452c34df6c7da89e3f2c8eed67f7d
Jucymara Lacet <jucymara.lacet@trampay.com>
2024-04-06 14:25:03 -0300

    Finalização aulas

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -4,6 +4,7 @@
 //e o bloco it, define um caso de teste.
 
 describe("Central de Atendimento ao Cliente TAT", function () {
+  const THREE_SECONDS_IN_MS=3000;
   // Antes de cada teste executa essa função
   beforeEach(function () {
     cy.visit("./src/index.html");
@@ -13,7 +14,10 @@ describe("Central de Atendimento ao Cliente TAT", function () {
   });
   it("preenche os campos obrigatorios e envia o formulário", function () {
     const longText =
-      " Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+      " Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+
+    cy.clock();
+
     cy.get("#firstName").type("Nome");
     cy.get("#lastName").type("Sobrenome");
     cy.get("#email").type("email@email.com");
@@ -23,8 +27,13 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.contains("button", "Enviar").click();
 
     cy.get(".success").should("be.visible");
+    cy.tick(THREE_SECONDS_IN_MS);
+    cy.get(".success").should("not.be.visible");
+
+
   });
   it("exibe mensagem de erro ao submeter o formulário com um email com formatação inválida", function () {
+    cy.clock();
     cy.get("#firstName").type("Nome");
     cy.get("#lastName").type("Sobrenome");
     cy.get("#email").type("email@email,com");
@@ -32,11 +41,21 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.contains("button", "Enviar").click();
 
     cy.get(".error").should("be.visible");
+    cy.tick(THREE_SECONDS_IN_MS);
+    cy.get(".error").should("not.be.visible");
+
   });
-  it("campo telefone continua vazio quando preenchido com valor não numérico", function () {
-    cy.get("#phone").type("abcdefghij").should("have.value", "");
-  });
+  Cypress._.times(3, function() {
+    it("campo telefone continua vazio quando preenchido com valor não numérico", function () {
+      cy.get("#phone")
+      .type("abcdefghij")
+      .should("have.value", "");
+    });
+  })
+
   it("exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário", function () {
+    cy.clock();
+
     cy.get("#firstName").type("Nome");
     cy.get("#lastName").type("Sobrenome");
     cy.get("#email").type("email@email.com");
@@ -45,6 +64,9 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     cy.contains("button", "Enviar").click();
 
     cy.get(".error").should("be.visible");
+    cy.tick(THREE_SECONDS_IN_MS);
+    cy.get(".error").should("not.be.visible");
+
   });
   it("preenche e limpa os campos nome, sobrenome, email e telefone", function () {
     cy.get("#firstName")
@@ -69,14 +91,20 @@ describe("Central de Atendimento ao Cliente TAT", function () {
       .should("have.value", "");
   });
   it("exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios", function () {
+    cy.clock();
     cy.contains("button", "Enviar").click();
 
     cy.get(".error").should("be.visible");
+    cy.tick(THREE_SECONDS_IN_MS);
+    cy.get(".error").should("not.be.visible");
   });
   it("envia o form com sucesso usando um comando customizado", function () {
+    cy.clock();
     cy.fillMandatoryFieldsAndSubmit();
 
     cy.get(".success").should("be.visible");
+    cy.tick(THREE_SECONDS_IN_MS);
+    cy.get(".error").should("not.be.visible");
   });
 
   // Testes em input tipo SELECT
@@ -151,5 +179,43 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     .click();
     //Verificação na mesma aba
     cy.contains("Talking About Testing").should("be.visible");
+  });
+  it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', function() {
+    cy.get('.success')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Mensagem enviada com sucesso.')
+      .invoke('hide')
+      .should('not.be.visible')
+    cy.get('.error')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Valide os campos obrigatórios!')
+      .invoke('hide')
+      .should('not.be.visible')
+  })
+  it("preenche a area de texto usando o comando invoke", function () {
+    //repeat para repetir a string
+    const longText = Cypress._.repeat('0123456789', 20)
+    cy.get('#open-text-area')
+    //invocar o valor de um input de texto e setamos
+    .invoke('val', longText)
+    .should('have.value', longText)
+  })
+  it("faz uma requisição HTTP", function(){
+    cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+    .should(function(response){
+      const {status, statusText, headers, body} = response
+      expect(status).to.equal(200)
+      expect(statusText).to.equal('OK')
+      expect(body).to.include('CAC TAT')
+    })
+  })
+  it.only("Encontrar o gato e demonstrar que ele está visível.", function () {
+      cy.get('#cat')
+        .invoke('show')
+        .should('be.visible')
   });
 });
--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,4 +1,7 @@
-it("Testa a página da política de privacidade de forma independente", function() {
-    cy.visit('./src/privacy.html')
-    cy.contains("Talking About Testing").should("be.visible");
+//A funcionalidade _.times() serve para você executar uma função de callback um certo número de vezes, onde o número de vezes é o primeiro argumento, e a função de callback é o segundo.
+Cypress._.times(3, function() {
+    it("Testa a página da política de privacidade de forma independente", function() {
+        cy.visit('./src/privacy.html')
+        cy.contains("Talking About Testing").should("be.visible");
+    })
 })
